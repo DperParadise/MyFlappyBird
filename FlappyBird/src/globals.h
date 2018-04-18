4766374fb86477a152713ca532e07ab5168d3676
Daniel Pérez <daniel.perezpa@hotmail.es>
2018-04-18 18:16:57 +0200

    Fix in header guard.

--- FlappyBird/src/globals.h
+++ FlappyBird/src/globals.h
@@ -1,6 +1,8 @@
-#ifndef _GLOBALS_H__
+#ifndef __GLOBALS_H__
 #define __GLOBALS_H__
 
 #define DELETE(x) if(x != nullptr)	{delete x; x = nullptr;}
 #define DELETE_ARRAY(x) if(x != nullptr) {delete[] x; x = nullptr;}
+
+
 #endif
